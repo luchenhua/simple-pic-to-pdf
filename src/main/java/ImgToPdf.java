a8f4eacf7718fe22bbcd89c3878b7194bdf70df6
Chenhua Lu <lu.chen.hua@wowoohr.com>
2016-09-29 18:24:57 +0800

    强制用jdk1.8编译运行

--- src/main/java/ImgToPdf.java
+++ src/main/java/ImgToPdf.java
@@ -1,14 +1,20 @@
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ImgToPdf {
 
     public void createPdf(String[] source, String dest) throws IOException, DocumentException {
+
+        File file = new File(dest);
+        file.getParentFile().mkdirs();
 
         Image img = Image.getInstance(source[0]);
         Document document = new Document(img);
@@ -18,9 +24,10 @@ public class ImgToPdf {
 
         for (String image : source) {
             img = Image.getInstance(image);
-            document.setPageSize(img);
+            document.setPageSize(PageSize.A4);
             document.newPage();
-            img.setAbsolutePosition(0, 0);
+            img.setAlignment(Element.ALIGN_CENTER);
+            img.scalePercent(100f);
             document.add(img);
         }
 
--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,21 +1,17 @@
 import com.itextpdf.text.DocumentException;
 
-import java.io.File;
 import java.io.IOException;
 
 public class SimpleTest {
 
-    public static final String DEST = "src/main/resources/pdf/multiple_images.pdf";
-
-    public static final String[] IMAGES = {
-            "src/main/resources/images/berlin2013.jpg",
-            "src/main/resources/images/javaone2013.jpg"
-    };
-
     public static void main(String[] args) throws IOException, DocumentException {
 
-        File file = new File(DEST);
-        file.getParentFile().mkdirs();
-        new ImgToPdf().createPdf(IMAGES, DEST);
+        String dest = "src/main/resources/pdf/multiple_images.pdf";
+        String[] images = {
+                "src/main/resources/images/berlin2013.jpg",
+                "src/main/resources/images/javaone2013.jpg"
+        };
+
+        new ImgToPdf().createPdf(images, dest);
     }
 }
